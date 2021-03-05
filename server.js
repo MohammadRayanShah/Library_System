0a13978371e06024368ad088926a8c0efb2c0c91
MohammadRayanShah <rayanshah379@gmail.com>
2021-03-05 19:34:47 +0530

    Port setup

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 const express=require("express");
 const app=express();
+
+
+app.set('view engine','ejs');
+
+app.get('/',(req,res)=>
+{
+    res.send("")
+});
+const port=process.env.PORT || 3000
+
+app.listen(port,()=>{console.log(`listening on port localhost:${port}`)});
