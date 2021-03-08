f88fa8bc1d8002cab25c9d4b75db754286250be5
MohammadRayanShah <rayanshah379@gmail.com>
2021-03-08 23:48:42 +0530

    ejs rendered

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express=require("express");
 const app=express();
+const path=require("path")
 
+app.set("view engine","ejs");
 
-app.set('view engine','ejs');
+app.use("/img", express.static(path.resolve(__dirname, "/images")));
+app.use("/css", express.static(path.resolve(__dirname, "/css")));
 
 app.get('/',(req,res)=>
 {
-    res.send("")
+    res.render('index');
 });
-const port=process.env.PORT || 3000
+const port=process.env.PORT || 5000
 
 app.listen(port,()=>{console.log(`listening on port localhost:${port}`)});
